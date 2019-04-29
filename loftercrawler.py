aa9356c0b91dc3c0a94c8aa0a180a06c8abd05bb
BYJRK <suomikoto@outlook.com>
2019-04-30 00:03:17 +0800

    进行了一些修改，但是还有问题
    1. 添加和修改了ArgumentParser的一些参数；
    2. 试图让输入的domain更加灵活，覆盖多种情况，但是目前正则表达式部分有问题
    3. 添加了下载图片时的进度条

--- loftercrawler.py
+++ loftercrawler.py
@@ -3,28 +3,34 @@ from utils import *
 import utils
 import argparse
 import time
+from tqdm import tqdm
 
-parser = argparse.ArgumentParser()
-parser.add_argument('domain', help='domain name or post link')
-parser.add_argument('-max', '--max_page', default=160, type=int,
-                    help='maximum page amount (default = 160)')
-parser.add_argument('-start', '--start_page', default=1, type=int,
-                    help='start searching from this page')
-parser.add_argument('-dir', '--directory',
-                    help='save the downloaded images to this local folder')
-parser.add_argument('--max_threads', default=8, type=int)
-parser.add_argument('-r', '--replace', default=False, type=bool,
-                    help='replace the existing files with the same name (default = False)')
+parser = argparse.ArgumentParser(description='爬取 Lofter 上面指定博客或贴子的图片')
+parser.add_argument('domain', help='要爬取的博客域名或贴子链接')
+parser.add_argument('--max', default=160,
+                    type=int, help='最大爬取页数（默认为 160）')
+parser.add_argument('--start', default=1, type=int, help='起始页（默认为 1）')
+parser.add_argument('--end', default=-1, type=int, help='终止页（默认为 -1，表示忽略）')
+parser.add_argument('--dir', help='图片存放路径（默认会从 domain 获取合适的文件名）')
+parser.add_argument('--max_threads', default=8, type=int, help='最大线程数（默认为 8）')
+parser.add_argument('--replace', default=False, type=bool,
+                    help='是否覆盖已存在的文件（默认为否）')
 parser.add_argument('--timeout', default=8, type=float,
-                    help='request timeout (second, default = 8)')
+                    help='超时时间（默认为 8 秒）')
+parser.add_argument('--cache-count', default=10,
+                    type=int, help='缓存网页的数量（默认为 10）')
 
 
 def post_links_in_page_range():
     """get all post links in a given page range"""
     # post links to find image links inside
     post_links = []
-    start_page = args.start_page
-    end_page = get_end_page_number(args.domain, start_page, args.max_page)
+    start_page = args.start
+    if args.end != -1:
+        assert args.end > args.start
+        end_page = args.end
+    else:
+        end_page = get_end_page_number(args.domain, start_page, args.max)
     print(f'Searching from page {start_page} to {end_page}')
 
     pool = Pool(processes=args.max_threads)
@@ -36,7 +42,7 @@ def post_links_in_page_range():
     pool.join()
     print(f'Found {len(post_links)} posts.')
     return post_links
-    
+
 
 def image_links_in_post_links(post_links):
     """find all image links from a list of post links"""
@@ -50,33 +56,40 @@ def image_links_in_post_links(post_links):
     pool.close()
     pool.join()
     stop = time.time()
-    print(f'Found {len(image_links)} images. Elapsed time: {stop - start:.4f} seconds.')
+    print(
+        f'Found {len(image_links)} images. Elapsed time: {stop - start:.4f} seconds.')
     return image_links
 
 
 def download_images_from_links(image_links):
     """download all images in image links"""
+    pbar = tqdm(total=len(image_links), ascii=True)
     print('Start downloading...')
     start = time.time()
     failed_links = []
     results = []
+
+    def download_callback(link):
+        results.append(link)
+        pbar.update()
+
     pool = Pool(processes=args.max_threads)
     for link in image_links:
-        pool.apply_async(download, args=(link, args.directory / get_filename(link), args.replace),
-                         callback=results.append)
+        pool.apply_async(download, args=(link, args.dir / get_filename(link), args.replace),
+                         callback=download_callback)
     pool.close()
     pool.join()
-    for link in (link for link in results if link):
-        failed_links.append(link)
+    failed_links.extend([link for link in results if link])
     stop = time.time()
-    print(f'Downloaded {len(image_links) - len(failed_links)} images. Elapsed time is {stop - start:.4f} seconds.')
+    print(
+        f'Downloaded {len(image_links) - len(failed_links)} images. Elapsed time is {stop - start:.4f} seconds.')
     if failed_links:
         print(f'{len(failed_links)} image(s) failed. Retrying...')
         pool = Pool(processes=args.max_threads)
         results = []
         for link in failed_links:
             # 3 times the original timeout, force replace
-            pool.apply_async(download, args=(link, args.directory / get_filename(link), True, args.timeout * 3),
+            pool.apply_async(download, args=(link, args.dir / get_filename(link), True, args.timeout * 3),
                              callback=results.append)
         pool.close()
         pool.join()
@@ -84,7 +97,8 @@ def download_images_from_links(image_links):
         for link in (link for link in results if link):
             unavailable_links.append(link)
         if unavailable_links:
-            print(f'{len(unavailable_links)} not available. Please download them later.')
+            print(
+                f'{len(unavailable_links)} not available. Please download them later.')
             for link in unavailable_links:
                 print(link)
 
@@ -93,10 +107,10 @@ def crawl_domain():
     """find all images in a specific page range and download them"""
     print(f'Start crawling {args.domain}.lofter.com')
     # check folder
-    if not args.directory:
-        args.directory = get_domain_title(args.domain)
-    args.directory = Path(args.directory)
-    check_folder(args.directory)
+    if not args.dir:
+        args.dir = get_domain_title(args.domain)
+    args.dir = Path(args.dir)
+    check_folder(args.dir)
     # find all posts
     post_links = post_links_in_page_range()
     # find all image links to download
@@ -107,15 +121,16 @@ def crawl_domain():
 
 def crawl_post(post_link):
     """find all images in a given post and download them"""
-    if not args.directory:
-        args.directory = args.domain.split('/')[-1]
-    args.directory = Path(args.directory)
-    check_folder(args.directory)
-    print(f'folder: #{args.directory}#')
-    
+    if not args.dir:
+        args.dir = args.domain.split('/')[-1]
+    args.dir = Path(args.dir)
+    check_folder(args.dir)
+    print(f'folder: #{args.dir}#')
+
     image_links = get_image_links_in_post(args.domain)
     print(f"Found {len(image_links)} images.")
     download_images_from_links(image_links)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -127,6 +142,10 @@ if __name__ == '__main__':
     # post link looks like domain.lofter.com/post/...
     if re.search(r'\.lofter\.com/post/', args.domain):
         crawl_post(args.domain)
-    else:
+    args.domain = re.search(
+        r'(?<=http://)(?P<domain>[^.]+)\.lofter\.com/?', args.domain).group('domain')
+    if args.domain:
         crawl_domain()
+    else:
+        raise Exception("输入的域名有误。")
     print('Downloading finished. Goodbye!')
